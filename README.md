2a3cc960e3698e6f7f36827621bb0b8973175a77
Dankaki <daniyar.akizhanov@gmail.com>
2021-02-01 23:32:43 +0600

    Added a Readme and changed the UI

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a simple epub reader made with TornadoFX Kotlin plugin.
+
+It utilizes epublib (https://github.com/psiegman/epublib) to parse the epub files.
+
+Right now, only the book manager/menu is implemented. **The actual reader isn't ready yet.**
+
+Maven 3.6.3 / Kotlin 1.4.20 / jdk 1.8
--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -9,18 +9,25 @@ import javafx.stage.FileChooser
 import nl.siegmann.epublib.domain.Book
 import nl.siegmann.epublib.epub.EpubReader
 import tornadofx.*
+import java.awt.Desktop
 import java.io.File
 import java.io.FileInputStream
+import java.net.URI
 
+/**
+ * The class for storing book data in the main menu
+ */
 class BookModel(name: String, author: String, path: String) {
-    private val nameProperty = SimpleStringProperty(
-            if (name.length < 35) name else name.subSequence(0, 32).toString() + "..."
-    )
+    //    private val nameProperty = SimpleStringProperty(
+//            if (name.length < 35) name else name.subSequence(0, 32).toString() + "..."
+//    )
+    private val nameProperty = SimpleStringProperty(name)
     var name: String by nameProperty
 
-    private val authorProperty = SimpleStringProperty(
-            if (author.length < 35) author else author.subSequence(0, 32).toString() + "..."
-    )
+    //    private val authorProperty = SimpleStringProperty(
+//            if (author.length < 35) author else author.subSequence(0, 32).toString() + "..."
+//    )
+    private val authorProperty = SimpleStringProperty(author)
     var author: String by authorProperty
 
     private val pathProperty = SimpleStringProperty(path)
@@ -40,12 +47,20 @@ class BookModel(name: String, author: String, path: String) {
     }
 }
 
+/**
+ * The main class for the menu window (view)
+ */
 class MainView : View("ePub Reader") {
     private val books = ArrayList<BookModel>().asObservable()
 
     override val root = hbox {
         useMaxWidth = true
+        // The books catalog
         listview(books) {
+            hboxConstraints {
+                useMaxWidth = true
+                hgrow = Priority.ALWAYS
+            }
             cellFormat {
                 graphic = vbox {
                     label(it.name) {
@@ -55,13 +70,18 @@ class MainView : View("ePub Reader") {
                     }
                     label(it.author)
                     onDoubleClick {
+                        // Moves the book to the first position in the list
                         books.remove(it)
                         books.add(0, it)
+
+                        // TODO: implement book opening
                         print(it.path)
                     }
                 }
             }
         }
+
+        // The menu
         vbox(10) {
             hboxConstraints {
                 paddingAll = 20
@@ -102,17 +122,24 @@ class MainView : View("ePub Reader") {
                     }
                 }
             }
-            button("Add folder") {
+//            button("Add folder") {
+//                useMaxWidth = true
+//                // TODO: implement the folder addition or remove the button
+//            }
+            button("This project on GitHub") {
                 useMaxWidth = true
-            }
-            button("Support the project") {
-                useMaxWidth = true
+                action {
+                    Desktop.getDesktop().browse(URI("https://github.com/dankaki/tornadofx_epub/"))
+                }
             }
 
         }
     }
 }
 
+/**
+ * The error message window class
+ */
 class ErrorWindow(message: String) : Fragment() {
     override val root = hbox {
         paddingAll = 5
